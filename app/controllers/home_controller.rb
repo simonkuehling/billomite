579e2a6c7d2dadaabc63b9ccbe480750d12c98b1
Simon Kühling <mail@simonkuehling.de>
2010-08-13 12:16:44 +0200

    billomat-customers with empty name-field now listed in step 4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -112,7 +112,11 @@ class HomeController < ApplicationController
 
     billomat_kunden = {}
     data['client'].each do |client|
-      billomat_kunden[client['id'][0]['content'].to_s] = client['name'].to_s
+      if client['name'].to_s == ''
+        billomat_kunden[client['id'][0]['content'].to_s] = client['client_number'].to_s + " (ohne Firmenname)"
+      else
+        billomat_kunden[client['id'][0]['content'].to_s] = client['name'].to_s
+      end
     end
     
     render :partial => 'schritt4', :object => billomat_kunden
